66064a86258e224b228dc55cdb29528cf8d1326e
Sueda Çiçekli <suedacicekli@gmail.com>
2024-11-16 21:00:35 +0300

    project file structure created

---
+++ src/pages/collers-page/CollersPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CollersPage() {
+  return (
+    <div>CollersPage</div>
+  )
+}
+
+export default CollersPage
---
+++ src/pages/index.ts
@@ -0,0 +1,3 @@
+export {default as QuizPage} from './quiz-page/QuizPage';
+export {default as LandingPage} from './landing-page/LandingPage';
+export {default as CollersPage} from './collers-page/CollersPage';
